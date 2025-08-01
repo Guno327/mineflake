aa6350acfa517a7b07f6feab82c6a2c3a42c0c58
gunnar <gunnarhovik@outlook.com>
2025-08-01 16:53:51 -0400

    setup vanilla version fetcher

---
+++ shell.nix
@@ -0,0 +1,18 @@
+{pkgs ? import <nixpkgs> {}}: let
+  inherit (pkgs) lib stdenv;
+in
+  pkgs.mkShell {
+    NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
+      stdenv.cc.cc
+    ];
+    NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
+
+    buildInputs = [
+      (pkgs.python3.withPackages
+        (p: [
+          p.jsondiff
+          p.requests
+          p.tqdm
+        ]))
+    ];
+  }
