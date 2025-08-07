67a2033c80edee2250aff8fa60700eb55275ddba
gunnar <gunnarhovik@outlook.com>
2025-08-07 19:14:34 -0400

    switched to aikairs flags as default

--- modules/vanilla.nix
+++ modules/vanilla.nix
@@ -54,7 +54,7 @@ in
 
       flags = mkOption {
         type = types.str;
-        default = "-Xmx1024M -Xms1024M";
+        default = "-Xms10G -Xmx10G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true";
         description = "Server launch flags";
       };
 
@@ -360,7 +360,7 @@ in
 
         script = ''
           cd ${cfg.dir}/${cfg.name}
-          exec ${cfg.java}/bin/java ${cfg.flags} -jar server.jar nogui
+          exec ${cfg.java}/bin/java ${cfg.flags} -jar server.jar --nogui
         '';
       };
     };
